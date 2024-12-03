4ac40f5c02ed8ce54d5a9d4bb4664c98251485c3
j00na <joonaviheria@gmail.com>
2024-12-03 22:20:50 +0200

    Refactor day 1

--- day1/main.py
+++ day1/main.py
@@ -10,14 +10,8 @@ def tansform_data(data):
     remove_whitespace = [elem.split() for elem in grouped_data]
     return remove_whitespace
 
-def get_lefts(data):
-    return [x[0] for x in data]
-
-def get_rights(data):
-    return [x[1] for x in data]
-
-def to_int(data):
-    return [int(x) for x in data]
+def lefts_and_rights(data):
+    return [int(x[0]) for x in data], [int(x[1]) for x in data]
 
 def get_distances(lefts, rights):
     lefts.sort()
@@ -25,12 +19,14 @@ def get_distances(lefts, rights):
     zips = list(zip(lefts, rights))
     return [abs(tuple[0] - tuple[1]) for tuple in zips]
 
+def similarity(lefts, rights):
+    return sum([rights.count(x) * x for x in lefts])
+
 if __name__ == "__main__":
     data = read_data('data.txt')
     data = tansform_data(data)
-    lefts = get_lefts(data)
-    lefts_as_ints = to_int(lefts)
-    rights = get_rights(data)
-    rights_as_ints = to_int(rights)
-    distances = get_distances(lefts_as_ints, rights_as_ints)
-    print(sum(distances))
+    lefts, rights = lefts_and_rights(data)
+    distances = get_distances(lefts, rights)
+    similarity = similarity(lefts, rights)
+    print(f"sum of distances is: {sum(distances)}")
+    print(f"sum of similarities is: {similarity}")
